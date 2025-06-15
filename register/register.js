54e79f4b18b7b79ee558384d71e297c867e68736
Rosyauto7410209 <64661777+Rosyauto7410209@users.noreply.github.com>
2025-06-14 19:58:23 -0600

    commit all

--- register/register.js
+++ register/register.js
@@ -1,82 +1,87 @@
+document.addEventListener("DOMContentLoaded", function () {
 let count = 1
+    const addParticipant = document.getElementById("add");
+    const submitButton = document.getElementById("submitButton");
 
+
+    addParticipant.addEventListener("click", function () {
+        let participantCount = countParticipants() + 1;
+
+        participantTemplate(participantCount);
+        
+    });
+    
+    submitButton.addEventListener("click", (event) => {
+        event.preventDefault();
+        successMessage = buildInfo()
+        successTemplate(successMessage);
+    })
+});
+
+function countParticipants() {
+  return document.querySelectorAll(".participant").length;
+}
 function participantTemplate(count) {
-  return `<section class="participant${count}">
-            <p>Participant ${count}</p>
-            <div class="item">
-              <label for="fname${count}"> First Name<span>*</span></label>
-              <input id="fname${count}" type="text" name="fname" value="" required />
-            </div>
-            <div class="item activities">
-              <label for="activity${count}">Activity #<span>*</span></label>
-              <input id="activity${count}" type="text" name="activity" />
-            </div>
-            <div class="item">
-              <label for="fee${count}">Fee ($)<span>*</span></label>
-              <input id="fee${count}" type="number" name="fee" />
-            </div>
-            <div class="item">
-              <label for="date${count}">Desired Date <span>*</span></label>
-              <input id="date${count}" type="date" name="date" />
-            </div>
-            <div class="item">
-              <p>Grade</p>
-              <select>
-                <option value="" selected disabled></option>
-                <option value="1">1st</option>
-                <option value="2">2nd</option>
-                <option value="3">3rd</option>
-                <option value="4">4th</option>
-                <option value="5">5th</option>
-                <option value="6">6th</option>
-                <option value="7">7th</option>
-                <option value="8">8th</option>
-                <option value="9">9th</option>
-                <option value="10">10th</option>
-                <option value="11">11th</option>
-                <option value="12">12th</option>
-              </select>
-            </div>
-          </section>`
+    addParticipant.insertAdjacentHTML("beforebegin", 
+        `<section class="participant">
+        <p>Participant ${count}</p>
+        <div class="item">
+            <label for="fname${count}"> First Name<span>*</span></label>
+            <input id="fname${count}" type="text" name="fname" required>
+        </div>
+        <div class="item activities">
+            <label for="activity${count}">Activity #<span>*</span></label>
+            <input id="activity${count}" type="text" name="activity">
+        </div>
+        <div class="item">
+            <label for="fee${count}">Fee ($)<span>*</span></label>
+            <input id="fee${count}" type="number" name="fee">
+        </div>
+        <div class="item">
+            <label for="date${count}">Desired Date <span>*</span></label>
+            <input id="date${count}" type="date" name="date">
+        </div>
+        <div class="item">
+            <p>Grade</p>
+            <select>
+                    <option selected="selected" value="" disabled="disabled"></option>
+                    <option value="1">1st</option>
+                    <option value="2">2nd</option>
+                    <option value="3">3rd</option>
+                    <option value="4">4th</option>
+                    <option value="5">5th</option>
+                    <option value="6">6th</option>
+                    <option value="7">7th</option>
+                    <option value="8">8th</option>
+                    <option value="9">9th</option>
+                    <option value="10">10th</option>
+                    <option value="11">11th</option>
+                    <option value="12">12th</option>
+            </select>
+        </div>
+    </section>`);
 }
-
-const addButton = document.querySelector('#add')
-
-function addParticipant() {
-  const newPartitipant = participantTemplate(++count)
-  addButton.insertAdjacentHTML('beforebegin', newPartitipant)
-}
-
-addButton.addEventListener('click', addParticipant)
-
-function totalFees() {
-  let feeElements = document.querySelectorAll('[id^=fee]')
-  feeElements = [...feeElements]
-
-  let total = 0
-  feeElements.forEach((ele) => {
-    total += parseInt(ele.value)
-  })
-  return total
-}
-
-const form = document.querySelector('#regForm')
-form.addEventListener('submit', submitForm)
-
 function submitForm(event) {
-  event.preventDefault()
-  const total = totalFees()
-  const name = document.querySelector('#adult_name').value
-  const info = {
-    name: name,
-    participants: count,
-    total: total
-  }
-  form.classList.add('hide')
-  const summary = document.querySelector('#summary')
-  summary.textContent = successTemplate(info)
+    event.preventDefault();
+    totalFees();
 }
+function totalFees() {
+    let feeElements = document.querySelectorAll("[id^=fee]");
+    console.log(feeElements);
+    feeElements = [...feeElements];
+    const feeTotal = feeElements.reduce((acc, fee) => acc + (Number(fee.value) || 0), 0);
 
+    return feeTotal;
+}
+function buildInfo() {
+    const name = document.getElementById("adult_name").value;
+    const participantCount = document.querySelectorAll("input[id^=fname]").length;
+    
+    return `Thank you ${name} for registering. You have registered ${participantCount} participant${participantCount !== 1 ? "s" : ""} and owe $${totalFees()} in Fees.`;
+}
 function successTemplate(info) {
-  return `Thank you ${info.name} for registering. You have registered ${info.participants} participants and owe $${info.total} in Fees.`
+    const form = document.querySelector("form");
+    form.classList.add('hide')
+    document.getElementById("summary").insertAdjacentHTML("beforeend", `<p>${info}</p>`);
+ 
 }
