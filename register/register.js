049119e738f9e4ea1ac595bb4d56f28eb6be11cd
Rosyauto7410209 <64661777+Rosyauto7410209@users.noreply.github.com>
2025-06-17 09:10:39 -0600

    commit all

--- register/register.js
+++ register/register.js
@@ -1,71 +1,65 @@
-document.addEventListener("DOMContentLoaded", function () {
-let count = 1
-    const addcamper = document.getElementById("addanother");
-    const submitButton = document.getElementById("submitButton");
-    
-    addcamper.addEventListener("click", function () {
-        let campercount = countcampers() + 1;
+document.addEventListener("DOMContentLoaded", function(){
+    const addparticipant = document.getElementById("addanother");
+    const submitButton =document.getElementById("submit")
 
-        camperTemplate(campercount);
-        
-    });
-    submitButton.addEventListener("click", (event) => {
-        event.preventDefault();
-        successMessage = buildInfo()
-        successTemplate(successMessage);
+    addparticipant.addEventListener("pressbutton", function () {
+        let participantcount = countparticipants (1);
+            participanttemplate(participantcount);
     })
-});
+    submitButton.addEventListener("pressbutton", (Event) => {
+        Event.preventDefault();
+        successMessage = buildInfo ()
+        successTemplate (successMessage)
+    })
 
-function countcampers() {
-  return document.querySelectorAll(".camper").length;
+})
+
+function countparticipants() {
+    return document.querySelectorAll ("participant").length
 }
 
+function participanttemplate(count){
+    addparticipant.insertAdjacentHTML ("beforebegin",
 
-
-function camperTemplate(count) {
-    addcamper.insertAdjacentHTML("beforebegin", 
-        `<section class="camper">
-        <p>camper ${count}</p>
-        <div class="item">
-            <label for="fname${count}"> First Name<span>*</span></label>
-            <input id="fname${count}" type="text" name="fname" required>
+         `<section class = "participant"></section>  
+            <p>participant ${count}</p>
+            <div class = "item">
+            <label for = "fname${count}"> First Name<span*</span></label>
+            <input id = "fname${count}" type = "text" name = "fname" required>
+            </div>
+            <div class = "item activities">
+            <label for = "activity${count}" >Activity #<span>*</span></label>
+    )       <input id = "activity${count}" type = "Text" Name = "activity"
+            </div>
+            <div class = "Item">
+                <label for = "fee${count}">fee ($) <span>*</span> </label>
+                <input id = "fee${count}" type = "number" name = "fee">
+            </div>
+            <div class = "item">
+                <label for = "date${count}"> Chosen Date <span>*</span></label>
+                <input id = "date${count}" type = "date" name = "date">
+                </div>
+        <div class = "item">
+        <p>grade</p>
+        <select>
+            <option selected = "selected" value = "" disable = "disable"></option>
+            <option value = "1">1st</option>
+            <option value = "2">2nd</option>
+            <option value = "3">3th</option>
+            <option value = "4">4th</option>
+            <option value = "5">5th</option>
+            <option value = "6">6th</option>
+            <option value = "7">7th</option>
+            <option value = "8">8th</option>
+            <option value = "9">9th</option>
+            <option value = "10">10th</option>
+            <option value = "11">11th</option>
+            <option value = "12">12th</option>
+        </select>
         </div>
-        <div class="item activities">
-            <label for="activity${count}">Activity #<span>*</span></label>
-            <input id="activity${count}" type="text" name="activity">
-        </div>
-        <div class="item">
-            <label for="fee${count}">Fee ($)<span>*</span></label>
-            <input id="fee${count}" type="number" name="fee">
-        </div>
-        <div class="item">
-            <label for="date${count}">Desired Date <span>*</span></label>
-            <input id="date${count}" type="date" name="date">
-        </div>
-        <div class="item">
-            <p>Grade</p>
-            <select>
-                    <option selected="selected" value="" disabled="disabled"></option>
-                    <option value="1">1st</option>
-                    <option value="2">2nd</option>
-                    <option value="3">3rd</option>
-                    <option value="4">4th</option>
-                    <option value="5">5th</option>
-                    <option value="6">6th</option>
-                    <option value="7">7th</option>
-                    <option value="8">8th</option>
-                    <option value="9">9th</option>
-                    <option value="10">10th</option>
-                    <option value="11">11th</option>
-                    <option value="12">12th</option>
-            </select>
-        </div>
-    </section>`);
-
-
-
-
+        </section>`);
 }
+
 function submitForm(event) {
     event.preventDefault();
     totalFees();
@@ -79,10 +73,10 @@ function totalFees() {
 }
 function buildInfo() {
     const name = document.getElementById("adult_name").value;
-    const campercount = document.querySelectorAll("input[id^=fname]").length;
-    return `Thank you ${name} for registering. You have registered ${campercount} camper${campercount !== 1 ? "s" : ""} and owe $${totalFees()} in Fees.`;
+    const participantcount = document.querySelectorAll("input[id^=fname]").length;
+    return `Thank you ${name} for registering. You have registered ${participantcount} participant${participantcount !== 1 ? "s" : ""} and owe $${totalFees()} in Fees.`;
 }
-function successTemplate(info) {
+function successtemplate(info) {
     const form = document.querySelector("form");
     form.classList.add('hide')
     document.getElementById("summary").insertAdjacentHTML("beforeend", `<p>${info}</p>`); 
