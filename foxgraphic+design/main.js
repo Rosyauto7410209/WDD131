857da45786a9d474bf96e149d06088597f9dd322
Rosyauto7410209 <64661777+Rosyauto7410209@users.noreply.github.com>
2025-07-19 13:39:08 -0600

    commit all

--- foxgraphic+design/main.js
+++ foxgraphic+design/main.js
@@ -1,28 +1,28 @@
   document.getelementByid ('sendmessage').addEventListener ('submit', function (event){
     event.preventDefault ();
 
-    // Clear previous errors
-    const errorElements = document.queryselectoall ('.error-message');
-    errorElements.forEach(el => el.style.display = 'none');
+
+    const unfinishedelements = document.queryselectoall ('.unfinishedmessage');
+    unfinishedelements.forEach(el => el.style.display = 'none');
 
     const name = document.getElementById('name').value.trim();
     const email = document.getElementById('email').value.trim();
     const message = document.getElementById('message').value.trim();
     let isvalid = true;
   if (name === '') {
-        document.getElementById('nameerror').textContent = 'Name is required';
-        document.getElementById('nameerror').style.display = 'block';
+        document.getElementById('nameisunfinished').textContent = 'Name is required';
+        document.getElementById('nameisunfinished').style.display = 'block';
         isvalid = false;}
 
     const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   if (email = '' || !emailPattern.test(email)) {
-        document.getElementById('emailerror').textContent = 'A valid email address is required';
-        document.getElementById('emailerror').style.display = 'block';
+        document.getElementById('emailisunfinished').textContent = 'A valid email address is required';
+        document.getElementById('emailisunfinished').style.display = 'block';
         isvalid = false;}
 
   if (message = ''){
-        document.getElementById('messageerror').textContent = 'A Message is required';
-        document.getElementById('messageerror').style.display = 'block';
+        document.getElementById('messageisunfinished').textContent = 'A Message is needed';
+        document.getElementById('messageisunfinished').style.display = 'block';
         isvalid = false;}
 
   if (isvalid){
